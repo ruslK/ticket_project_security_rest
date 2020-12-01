457afe3168a0c7f911dfe42a9fb0af1da1f3706b
Ruslan Kasymov <russkasymov@gmail.com>
2020-11-30 21:20:53 -0500

    adding more mock data

--- src/main/java/com/ticketsproject/bootstrap/DataGenerator.java
+++ src/main/java/com/ticketsproject/bootstrap/DataGenerator.java
@@ -37,16 +37,26 @@ public class DataGenerator implements CommandLineRunner {
 
         UserDTO u1 = new UserDTO("Job", "Misha", "job@misha.com", "password", true, "234523452345", roleService.findById(1L), Gender.FEMALE);
         UserDTO u2 = new UserDTO("Tina", "Ivanko", "tina@gmail.com", "password", true, "4356435643564356", roleService.findById(2L), Gender.FEMALE);
+        UserDTO u6 = new UserDTO("Coner", "White", "tina@gmail.com", "password", true, "4356435643564356", roleService.findById(2L), Gender.FEMALE);
         UserDTO u3 = new UserDTO("Steve", "Shapiro", "steve@shapiro.com", "password", true, "4353454356", roleService.findById(3L), Gender.FEMALE);
+        UserDTO u4 = new UserDTO("Ivanko", "Sveta", "steve@shapiro.com", "password", true, "4353454356", roleService.findById(3L), Gender.FEMALE);
+        UserDTO u5 = new UserDTO("Mariana", "Karson", "steve@shapiro.com", "password", true, "4353454356", roleService.findById(3L), Gender.FEMALE);
         userService.save(u1);
         userService.save(u2);
         userService.save(u3);
+        userService.save(u4);
+        userService.save(u5);
+        userService.save(u6);
 
         ProjectDTO p1 = new ProjectDTO("Spring MVC", "PR001", u2, LocalDate.now(), LocalDate.now().plusDays(25), "Project 1", Status.COMPLETE);
         ProjectDTO p2 = new ProjectDTO("Spring MVC 2", "PR002", u2, LocalDate.now(), LocalDate.now().plusDays(35), "Project 2", Status.IN_PROGRESS);
-        ProjectDTO p3 = new ProjectDTO("Spring MVC 3", "PR003", u2, LocalDate.now(), LocalDate.now().plusDays(45), "Project 3", Status.UAT_TEST);
+        ProjectDTO p3 = new ProjectDTO("Spring MVC 3", "PR003", u6, LocalDate.now(), LocalDate.now().plusDays(45), "Project 3", Status.UAT_TEST);
+        ProjectDTO p4 = new ProjectDTO("Spring MVC 4", "PR004", u6, LocalDate.now(), LocalDate.now().plusDays(45), "Project 3", Status.OPEN);
+        ProjectDTO p5 = new ProjectDTO("Spring MVC 5", "PR005", u2, LocalDate.now(), LocalDate.now().plusDays(45), "Project 3", Status.OPEN);
         projectService.save(p1);
         projectService.save(p2);
         projectService.save(p3);
+        projectService.save(p4);
+        projectService.save(p5);
     }
 }
