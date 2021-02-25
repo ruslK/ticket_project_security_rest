ad1bc0c127bb8938e8f49916d9c6d229618b4a02
Ruslan Kasymov <russkasymov@gmail.com>
2021-02-24 21:36:31 -0500

    Implement Logging with @Slf4j

--- src/main/java/com/ticketsproject/aspect/LoggingAspect.java
+++ src/main/java/com/ticketsproject/aspect/LoggingAspect.java
@@ -1,19 +1,17 @@
 package com.ticketsproject.aspect;
 
 import com.ticketsproject.exception.TicketingProjectException;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Aspect
 @Configuration
+@Slf4j
 public class LoggingAspect {
-
-    private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut("execution(* com.ticketsproject.controller.ProjectController.*(..)) " +
             "|| execution(* com.ticketsproject.controller.TaskController.*(..))")
@@ -23,21 +21,21 @@ public class LoggingAspect {
     @Before("anyControllerOperations()")
     public void anyBeforeControllerOperationsAdvice(JoinPoint joinPoint) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        logger.info("Before(User:{} - Method:{} - Parameter:{}",
+        log.info("Before(User:{} - Method:{} - Parameter:{}",
                 auth.getName(), joinPoint.getSignature().toShortString(), joinPoint.getArgs());
     }
 
     @AfterReturning(pointcut = "anyControllerOperations()", returning = "results")
     public void anyAfterReturningControllerOperationAdvice(JoinPoint joinPoint, Object results) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        logger.info("AfterReturning (User:{} - Method:{} - Parameter:{})",
+        log.info("AfterReturning (User:{} - Method:{} - Parameter:{})",
                 auth.getName(), joinPoint.getSignature().toShortString(), results.toString());
     }
 
     @AfterThrowing(pointcut = "anyControllerOperations()", throwing = "exception")
     public void anyAfterThrowingControllerOperationAdvise(JoinPoint joinPoint, TicketingProjectException exception) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        logger.info("AfterThrowing (User:{} - Method:{} - Exception:{})",
+        log.info("AfterThrowing (User:{} - Method:{} - Exception:{})",
                 auth.getName(), joinPoint.getSignature().toShortString(), exception.getLocalizedMessage());
     }
 
--- src/main/java/com/ticketsproject/aspect/PerformanceAspect.java
+++ src/main/java/com/ticketsproject/aspect/PerformanceAspect.java
@@ -1,18 +1,16 @@
 package com.ticketsproject.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
+@Slf4j
 public class PerformanceAspect {
-
-    Logger log = LoggerFactory.getLogger(PerformanceAspect.class);
 
     @Pointcut("@annotation(com.ticketsproject.annotation.ExecutionTime)")
     private void anyExecutionTimeOperation(){}
